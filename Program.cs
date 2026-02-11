d32ac05510fff149041b9d2967c0e9d63c50631f
Sar.Millie <Sar.Millie@S26-5CG538197M>
2026-02-11 12:20:29 +1100

    started module

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿// Program Name: 
-// Created on: 
-// Author: 
+// Created on: 10/02/26
+// Author: Sarrah
 
+// collecting name
+Console.WriteLine();
+Console.WriteLine("What is your name?: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Nice to meet you {name}!");
+
+// fav colour
+Console.WriteLine();
+Console.WriteLine("What is your favourite colour?: ");
+string colour = Console.ReadLine();
+
+if
+Console.WriteLine($" {colour}!");
 
